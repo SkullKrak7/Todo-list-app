5248032a8694f3348276c9572ef85b6ddb93a6dc
[Karthik Kagolanu] <[sai.kagolanu@yahoo.com]>
2024-12-15 22:56:32 +0000

    Edited functions import

--- cli.py
+++ cli.py
@@ -1,7 +1,4 @@
 from functions import write_todos, get_todos
-import FreeSimpleGUI
-
-FILEPATH = 'todos.txt'
 
 while True:
 
@@ -9,43 +6,43 @@ while True:
     user_action = user_action.strip()
 
     if "add" in user_action[0:3]:
-        todos = get_todos(FILEPATH)
+        todos = get_todos()
         todo = user_action[4:]
         todos.append(todo)
-        write_todos(FILEPATH, todos)
+        write_todos(todos)
 
     if "show" in user_action[0:4]:
-        todos = get_todos(FILEPATH)
+        todos = get_todos()
         for i, todo in enumerate(todos):
             todo.strip('\n')
             print(f"{i + 1}. {todo}")
 
     if "edit" in user_action[0:4]:
-        todos = get_todos(FILEPATH)
+        todos = get_todos()
         for i, todo in enumerate(todos):
             todo.strip('\n')
             print(f"{i + 1}. {todo}")
 
         index = int(input("Enter item number what you want to edit: ")) - 1
-        replace = input("Type what you want to replace with: ") m
+        replace = input("Type what you want to replace with: ")
 
         todos[index] = replace
 
-        write_todos(FILEPATH, todos)
+        write_todos(todos)
 
     if "complete" in user_action[0:8]:
-        todos = get_todos(FILEPATH)
+        todos = get_todos()
 
         index = int(input("Which item number do you want to mark as complete? "))
         todos.pop(index - 1)
 
-        write_todos(FILEPATH, todos)
+        write_todos(todos)
 
     if "exit" in user_action[0:4]:
 
         print("Your current list is:")
 
-        todos = get_todos(FILEPATH)
+        todos = get_todos()
 
         for i, todo in enumerate(todos):
             todo.strip('\n')
